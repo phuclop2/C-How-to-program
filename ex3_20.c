d9d06c4b1192456d84da6e5dc8e59aed0de64069
Phuc Ha <50400585+phuclop2@users.noreply.github.com>
2021-06-17 17:03:38 +0700

    Create ex3_20.c

---
+++ ex3_20.c
@@ -0,0 +1,39 @@
+// Exercise 3.20 Solution
+// Salary Calculator 
+
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{	
+	float hours; // total hour worked
+	float rate; // hourly pay rate
+	float salary; // gross pay
+
+	// get first employee's hours worked
+	printf("%s", "Enter number of hours worked (-1 to end): ");
+	scanf("%f", &hours);
+
+	// loop until sentinel value read from user
+	while (hours != -1.0) {
+
+		// get hourly rate
+		printf("%s", "Enter hourly rate of the worker ($00.00): ");
+		scanf("%f", &rate);
+
+		// if employee worked less than 40 hours
+		if (hours <= 40) {
+			salary = hours * rate;
+		} // end if
+		else { // empoyee worked over 40 hours
+			salary = 40.0 * rate + (hours - 40.0) * rate * 1.5; 
+		} // end else
+
+		// display gross pay
+		printf("Salary is $%.2f\n\n", salary);
+
+		// prompt for next employess's data
+		printf("%s", "Enter number of hours worked (-1 to end): ");
+		scanf("%f", &hours);
+	}
+} // end function main
