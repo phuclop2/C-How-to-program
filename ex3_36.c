159732670e503777d55d75bf9b9cb16f647d3575
Phuc Ha <50400585+phuclop2@users.noreply.github.com>
2021-06-22 23:30:18 +0700

    Create ex3_36.c

---
+++ ex3_36.c
@@ -0,0 +1,26 @@
+// Exercise 3.36 Solution
+// Armstrong Numbers
+
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{	
+	int number = 100; // number to count
+	int a, b, c, temp;
+
+	while (number <= 999) {
+
+		// find three digits 
+		a = number / 100;
+		b = (number % 100) / 10;
+		c = (number % 100) % 10;
+		temp = (a * a * a) + (b * b * b) + (c * c * c);
+
+		if (temp == number) { // check for Armstrong Number
+			printf("%d\n", number);
+		} // end if
+
+		++number; // increment number
+	}
+} // end function main
