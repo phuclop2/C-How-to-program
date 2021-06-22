488e58edd020a034eedd261299dc4d2c3605a6f9
Phuc Ha <50400585+phuclop2@users.noreply.github.com>
2021-06-23 00:39:13 +0700

    Create ex3_43.c

---
+++ ex3_43.c
@@ -0,0 +1,24 @@
+// Exercise 3.43 Solution
+// Side of a Triangle
+
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{	
+	double a; // first number
+	double b; // second number
+	double c; // third number
+
+	// input 3 number
+	printf("%s", "Enter three doubling point numbers: ");
+	scanf("%lf %lf %lf", &a, &b, &c);
+
+	// use Pythagorean Theorem
+	if (c* c == a * a + b * b) {
+		printf("The three numbers could be the sides of a triangle\n");
+	} // end if
+	else {
+		printf("The three numbers probably are not the sides of a triangle\n");
+	} // end else
+} // end function main
